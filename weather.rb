398256b19e6cdd5ec33d2def25f3213d98475b1e
Nancerss <nancy.alquicirauc@gmail.com>
2018-07-30 13:59:54 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -11,8 +11,6 @@ def location(weather)
   puts weather[:city]
 end
 
-
-
 # 2. Using the location method as a guide, write a  method called "description" so it returns the description from the weather hash. 
 
 
