059640c8de0092604683af790452cd557a9b21b9
Nancerss <nancy.alquicirauc@gmail.com>
2018-07-30 13:57:30 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -14,7 +14,6 @@ def location(weather)
 	  :temperature_farenheit => 66.3 }
 end
 
-location(weather)
 
 # 2. Using the location method as a guide, write a  method called "description" so it returns the description from the weather hash. 
 
